6dc640b55fede7bf4a2b73787a3c293d12a01614
Cassiano Viana <cassiano.pvianna@gmail.com>
2021-03-26 13:02:23 -0300

    Simplificnado parte de testes

--- frontend/components/ScoreFunctionTestDialog.vue
+++ frontend/components/ScoreFunctionTestDialog.vue
@@ -3,9 +3,11 @@
     :visible.sync="visible"
     top="10px"
     width="95%"
+    id="score-fn-dialog"
     title="Playground para teste de função de escore"
   >
-    <score-function-test-form v-model="mechanic" :showTestFunction="true" />
+    <div style="height: 10px; width: 100px"></div>
+    <score-function-test-form v-model="mechanic" :showTestCases="true" />
   </el-dialog>
 </template>
 <script lang="ts">
@@ -28,7 +30,7 @@ import Vue from "vue";
   },
 })
 export default class ScoreFunctionTestDialog extends Vue {
-  visible: boolean = false;
+  visible: boolean = true;
   @VModel() mechanic!: Mechanic;
 
   show() {
@@ -37,4 +39,27 @@ export default class ScoreFunctionTestDialog extends Vue {
 }
 </script>
 <style lang="scss">
+#score-fn-dialog {
+  max-height: 100vh i !important;
+  //border: 1px solid green;
+  .el-dialog__header {
+    background: white;
+    width: 100%;
+    height: 60px;
+    z-index: 100;
+    top: 0;
+    left: 0;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 2px 4px 0px !important;
+    padding: 20px;
+    position: relative;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+  }
+  .el-dialog__body {
+    max-height: 90vh;
+    overflow-y: scroll;
+    //border: 1px solid red;
+  }
+}
 </style>
