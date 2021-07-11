32c7d6071b83efb838100ad7285f52d0d740ef96
Cassiano Viana <cassiano.pvianna@gmail.com>
2021-07-10 22:03:32 -0300

    Usando imagem do ubuntu para buildar container docker

--- frontend/types/RFunctionCode.ts
+++ frontend/types/RFunctionCode.ts
@@ -12,7 +12,7 @@ export const rPredefinedFunctions: RFunctionCode[] = [
         name: 'Teste de normalidade Shapiro-Wilk',
         description: 'Testa se um determinado conjunto de itens apresenta distribuição normal.',
         code: 'shapiro.test($1)',
-        howToInterpret: 'Se o p-value for maior que o intervalo [0.01, 0.1] então os dados devem ter distribuição normal.',
+        howToInterpret: 'Se o p-value for maior que 0.1 então os dados seguem a distribuição normal.',
         moreDescription: `Pra a maioria das aplicações, você pode aceitar H0 (distribuição é normal) com segurança se p > 0,1 e rejeitar H0 com segurança se p <0,01. Para valores de p neste intervalo [0,01,0,1], pode ser uma boa ideia coletar mais dados.`,
         source: 'https://www.programmingr.com/shapiro-wilk-test-in-r/'
     },
